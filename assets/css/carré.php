0d6f9495bee178ad7d21ab4cb0718c59e9427ef9
Hrivaux <hugorivaux@gmail.com>
2022-12-02 14:16:51 +0100

    portfolio

---
+++ assets/css/carré.php
@@ -0,0 +1,18 @@
+<?php
+
+function carre($nb1, $resultat)
+{
+
+$resultat $nb1*$nb1 ;
+return $resultat
+}
+
+function pseudo($prenom, $nom)
+{
+return substr($nom,0,4) . substr($prenom,0,4)
+}
+
+function val_carre_cube
+
+
+?>
